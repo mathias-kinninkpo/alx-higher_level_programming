1a6428749950bdfd1df7985e56b6842c1e4ef8d1
mathias-kinninkpo <kinninkpomathias@gmail.com>
2023-10-31 01:45:41 +0100

    "adding"

---
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+str = "Python is an interpreted, interactive, object-oriented programming\
+ language that combines remarkable power with very clear syntax"
+print(str.split(' object-oriented programming')[1].split(' with')[0])
+
