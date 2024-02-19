312c023ce0fee78c498baf5aecd10829d99813b0
mathias-kinninkpo <kinninkpomathias@gmail.com>
2024-02-19 19:27:53 +0100

    modification model state insert

--- 0x0F-python-object_relational_mapping/11-model_state_insert.py
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -1,25 +1,21 @@
 #!/usr/bin/python3
-"""Inserts into State obj from db"""
+# Adds the State object "Louisiana" to the database hbtn_0e_6_usa.
+# Usage: ./11-model_state_insert.py <mysql username> /
+#                                   <mysql password> /
+#                                   <database name>
 import sys
 from sqlalchemy import create_engine
-from sqlalchemy.orm import Session
-from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
+from model_state import State
 
-
-def insert_to_state_obj():
+if __name__ == "__main__":
     engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
                            .format(sys.argv[1], sys.argv[2], sys.argv[3]),
                            pool_pre_ping=True)
-    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
 
-    session = Session(engine)
-    state = State(name='Louisiana')
-
-    session.add(state)
+    louisiana = State(name="Louisiana")
+    session.add(louisiana)
     session.commit()
-    print(state.id)
-
-    session.close()
-
-if __name__ == "__main__":
-    insert_to_state_obj()
+    print(louisiana.id)
