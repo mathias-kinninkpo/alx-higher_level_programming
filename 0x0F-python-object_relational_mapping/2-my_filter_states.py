8c8e644d04b13084483dd62f18832949f6c26e9a
mathias-kinninkpo <kinninkpomathias@gmail.com>
2024-02-19 19:25:49 +0100

    modification filter state

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,36 +1,17 @@
 #!/usr/bin/python3
-"""Display name argument of states table"""
-import MySQLdb
+# Displays all values in the states table of the database hbtn_0e_0_usa
+# whose name matches that supplied as argument.
+# Usage: ./2-my_filter_states.py <mysql username> \
+#                                <mysql password> \
+#                                <database name> \
+#                                <state name searched>
 import sys
-
-
-def filter_names():
-    """Takes arguments argv to list from database
-    Only lists with states that matches name argument
-
-    Arguments:
-        argv[1]: mysql username
-        argv[2]: mysql password
-        argv[3]: database name
-        argv[4]: state name
-    """
-    db = MySQLdb.connect(host="localhost",
-                         port=3306,
-                         user=sys.argv[1],
-                         passwd=sys.argv[2],
-                         db=sys.argv[3])
-
-    cur = db.cursor()
-
-    cur.execute("SELECT * FROM states WHERE BINARY name='{:s}'\
-                ORDER BY id ASC".format(sys.argv[4]))
-    rows = cur.fetchall()
-    for i in rows:
-        print(i)
-
-    cur.close()
-    db.close()
-
+import MySQLdb
 
 if __name__ == "__main__":
-    filter_names()
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    c = db.cursor()
+    c.execute("SELECT * \
+                 FROM `states` \
+                WHERE BINARY `name` = '{}'".format(sys.argv[4]))
+    [print(state) for state in c.fetchall()]
