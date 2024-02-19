5c864c85191d08b95911ca0712d55f2456fd1ed1
mathias-kinninkpo <kinninkpomathias@gmail.com>
2024-02-19 19:27:03 +0100

    modification model state fetch first

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,27 +1,22 @@
 #!/usr/bin/python3
-"""List first State objects from db"""
+# Prints the first State object from the database hbtn_0e_6_usa.
+# Usage: ./8-model_state_fetch_first.py <mysql username> /
+#                                       <mysql password> /
+#                                       <database name>
 import sys
 from sqlalchemy import create_engine
-from sqlalchemy.orm import Session
-from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
+from model_state import State
 
-
-def list_first_state_obj():
+if __name__ == "__main__":
     engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
                            .format(sys.argv[1], sys.argv[2], sys.argv[3]),
                            pool_pre_ping=True)
-    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
 
-    session = Session(engine)
-
-    f = session.query(State).first()
-
-    if f:
-        print("{}: {}".format(f.__dict__['id'], f.__dict__['name']))
-    else:
+    state = session.query(State).order_by(State.id).first()
+    if state is None:
         print("Nothing")
-
-    session.close()
-
-if __name__ == "__main__":
-    list_first_state_obj()
+    else:
+        print("{}: {}".format(state.id, state.name))
