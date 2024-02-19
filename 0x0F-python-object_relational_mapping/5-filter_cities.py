3a5341bc7126daf289271c36c9f7034dd0306166
mathias-kinninkpo <kinninkpomathias@gmail.com>
2024-02-19 19:26:32 +0100

    modification filter city

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,42 +1,19 @@
 #!/usr/bin/python3
-"""Displays all cities of arguments state"""
-import MySQLdb
+# Displays all cities of a given state from the
+# states table of the database hbtn_0e_4_usa.
+# Safe from SQL injections.
+# Usage: ./5-filter_cities.py <mysql username> \
+#                             <mysql password> \
+#                             <database name> \
+#                             <state name searched>
 import sys
-
-
-def list_cities():
-    """Takes arguments argv to list from database
-    Only lists with states that matches name argument
-
-    Arguments:
-        argv[1]: mysql username
-        argv[2]: mysql password
-        argv[3]: database name
-    """
-    if len(sys.argv) == 5:
-        db = MySQLdb.connect(host="localhost",
-                             port=3306,
-                             user=sys.argv[1],
-                             passwd=sys.argv[2],
-                             db=sys.argv[3])
-
-        cur = db.cursor()
-
-        cur.execute("SELECT cities.name FROM cities\
-                    JOIN states ON cities.state_id = states.id\
-                    AND states.name = '{:s}'\
-                    ORDER BY cities.id ASC".format(sys.argv[4]))
-
-        rows = cur.fetchall()
-
-        res = []
-        for i in rows:
-            res.append(i[0])
-
-        print(", ".join(res))
-
-        cur.close()
-        db.close()
+import MySQLdb
 
 if __name__ == "__main__":
-    list_cities()
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    c = db.cursor()
+    c.execute("SELECT * FROM `cities` as `c` \
+                INNER JOIN `states` as `s` \
+                   ON `c`.`state_id` = `s`.`id` \
+                ORDER BY `c`.`id`")
+    print(", ".join([ct[2] for ct in c.fetchall() if ct[4] == sys.argv[4]]))
