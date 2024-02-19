02cb01c0bc6e585d18b5f9708799fd504cec7cdb
mathias-kinninkpo <kinninkpomathias@gmail.com>
2024-02-19 19:28:35 +0100

    modification city fetch by state

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,28 +1,22 @@
 #!/usr/bin/python3
-"""Displays all City obj from db"""
+# Lists all City objects from the database hbtn_0e_14_usa.
+# Usage: ./14-model_city_fetch_by_state.py <mysql username> /
+#                                          <mysql password> /
+#                                          <database name>
 import sys
 from sqlalchemy import create_engine
-from sqlalchemy.orm import Session
-from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
+from model_state import State
 from model_city import City
 
-
-def list_city_obj():
+if __name__ == "__main__":
     engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
                            .format(sys.argv[1], sys.argv[2], sys.argv[3]),
                            pool_pre_ping=True)
-    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
 
-    session = Session(engine)
-
-    rows = session.query(State, City).join(City).all()
-
-    for i in rows:
-        print("{}: ({}) {}".format(i[0].__dict__['name'],
-                                   i[1].__dict__['id'],
-                                   i[1].__dict__['name']))
-
-    session.close()
-
-if __name__ == "__main__":
-    list_city_obj()
+    for city, state in session.query(City, State) \
+                              .filter(City.state_id == State.id) \
+                              .order_by(City.id):
+        print("{}: ({}) {}".format(state.name, city.id, city.name))
