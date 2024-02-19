7382f70c1f33b20f410664f88f8e8a9e554c7681
mathias-kinninkpo <kinninkpomathias@gmail.com>
2024-02-19 19:27:36 +0100

    modification model state my get

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -1,33 +1,27 @@
 #!/usr/bin/python3
-"""List all State objects containing argument from db"""
+# Lists the State object with the name passed as argument
+# from the database hbtn_0e_6_usa.
+# Usage: ./10-model_state_my_get.py <mysql username> /
+#                                   <mysql password> /
+#                                   <database name>
+#                                   <state name searched>
 import sys
 from sqlalchemy import create_engine
-from sqlalchemy.orm import Session
-from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
+from model_state import State
 
-
-def list_arg_state_obj():
+if __name__ == "__main__":
     engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
                            .format(sys.argv[1], sys.argv[2], sys.argv[3]),
                            pool_pre_ping=True)
-    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
 
-    session = Session(engine)
-
-    rows = session.query(State).all()
-
-    res = ""
-
-    for i in rows:
-        if sys.argv[4] in i.__dict__['name']:
-            res = i.__dict__['id']
-
-    if res != "":
-        print(res)
-    else:
-        print("Not Found")
-
-    session.close()
-
-if __name__ == "__main__":
-    list_arg_state_obj()
+    found = False
+    for state in session.query(State):
+        if state.name == sys.argv[4]:
+            print("{}".format(state.id))
+            found = True
+            break
+    if found is False:
+        print("Not found")
